3fc4f7d2f810978f5c15e2d29ac1c40013315a17
lilxak <zakben45@gmail.com>
2023-12-10 21:33:20 +0100

    Remove useless logout template

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,5 @@ from .views import RegisterFormView
 urlpatterns = [
   path('register', RegisterFormView.as_view(), name='register'),
   path('login', LoginView.as_view(template_name='authentication/login.html'), name='login'),
-  path('logout', LogoutView.as_view(template_name='authentication/logout.html'), name='logout'),
+  path('logout', LogoutView.as_view(), name='logout'),
 ]
