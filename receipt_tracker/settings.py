45b6685f58c5e47178a90615d69a935ab7f840a3
Zaki <33980130+benaissazaki@users.noreply.github.com>
2023-12-11 21:57:25 +0100

    Create vercel settings (#4)
    * First configurations
    
    * Downgrade from Django 5 to 4.2 for vercel compat
    
    * Update Django version in requirements.txt
    
    * Install psycopg
    
    * Install psycopg binary
    
    * Update env example file

--- receipt_tracker/settings.py
+++ receipt_tracker/settings.py
@@ -30,7 +30,6 @@ DEBUG = env('DEBUG') == 'True'
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', None, [])
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -77,13 +76,25 @@ WSGI_APPLICATION = 'receipt_tracker.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'URL': env('DATABASE_URL'),
+            'NAME': env('PGDATABASE'),
+            'USER': env('PGUSER'),
+            'PASSWORD': env('PGPASSWORD'),
+            'HOST': env('PGHOST'),
+            'PORT': env('PGPORT'),
+        }
+    }
 
 
 # Password validation
