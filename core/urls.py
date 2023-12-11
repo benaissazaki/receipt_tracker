8b543a975f2131ce15f4e3ca482b3a6d9b426378
lilxak <zakben45@gmail.com>
2023-12-11 10:53:55 +0100

    Add tests for receipt_list view

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from django.contrib.auth.decorators import login_required
 from core.views import ReceiptListView
 
 urlpatterns = [
-    path('receipt', login_required(ReceiptListView.as_view(), login_url=reverse_lazy('login')), name='receipt_list')
+    path('receipt', login_required(ReceiptListView.as_view(),
+         login_url=reverse_lazy('login')), name='receipt_list')
 ]
