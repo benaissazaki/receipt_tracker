d9cdd152b493e99985f544cf83a7bbed1f0c8bc3
lilxak <zakben45@gmail.com>
2023-12-11 14:14:47 +0100

    Create receipt_delete view

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from django.db.models.query import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import UpdateView, CreateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from .models import Receipt
 
 
@@ -44,6 +44,7 @@ class ReceiptUpdateView(UpdateView):
     def get_success_url(self) -> str:
         return reverse_lazy('receipt_detail', args=[self.get_object().pk])
 
+
 class ReceiptCreateView(CreateView):
     model = Receipt
     fields = ['store_name', 'date_of_purchase', 'item_list', 'total_amount']
@@ -53,6 +54,22 @@ class ReceiptCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
-        
+
     def get_success_url(self) -> str:
         return reverse_lazy('receipt_detail', args=[self.object.pk]) # type: ignore
+
+
+class ReceiptDeleteView(DeleteView):
+    model = Receipt
+    template_name_suffix = '_delete'
+    context_object_name = 'receipt'
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        receipt = self.get_object()
+        if receipt.user != request.user:  # type: ignore
+            return HttpResponseForbidden('You do not have the permission to delete this receipt')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('receipt_list')
