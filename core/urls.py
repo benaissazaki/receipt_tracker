8305340571112fdd4c57f8334b173d36caea81a0
lilxak <zakben45@gmail.com>
2023-12-11 12:07:51 +0100

    Create receipt update view

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, reverse_lazy
 from django.contrib.auth.decorators import login_required
 
-from core.views import ReceiptListView, ReceiptDetailView
+from core.views import ReceiptListView, ReceiptDetailView, ReceiptUpdateView
 
 urlpatterns = [
     path('receipt', login_required(ReceiptListView.as_view(),
          login_url=reverse_lazy('login')), name='receipt_list'),
     path('receipt/<int:pk>', login_required(ReceiptDetailView.as_view(),
-         login_url=reverse_lazy('login')), name='receipt_detail')
+         login_url=reverse_lazy('login')), name='receipt_detail'),
+    path('receipt/<int:pk>/edit', login_required(ReceiptUpdateView.as_view(),
+         login_url=reverse_lazy('login')), name='receipt_update')
+    
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpRequest, HttpResponse, HttpResponseForbidden
 from django.db.models.query import QuerySet
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import UpdateView
 from .models import Receipt
+
 
 class ReceiptListView(ListView):
     model = Receipt
@@ -11,14 +14,32 @@ class ReceiptListView(ListView):
 
     def get_queryset(self) -> QuerySet[Receipt]:
         return Receipt.objects.filter(user=self.request.user).order_by('date_of_purchase')
-    
+
+
 class ReceiptDetailView(DetailView):
     model = Receipt
     context_object_name = 'receipt'
-    
+
     def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         receipt = self.get_object()
-        if receipt.user != request.user: # type: ignore
+        if receipt.user != request.user:  # type: ignore
             return HttpResponseForbidden('You do not have the permission to access this receipt')
-        
+
         return super().dispatch(request, *args, **kwargs)
+
+
+class ReceiptUpdateView(UpdateView):
+    model = Receipt
+    fields = ['store_name', 'date_of_purchase', 'item_list', 'total_amount']
+    template_name_suffix = '_update'
+    context_object_name = 'receipt'
+
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        receipt = self.get_object()
+        if receipt.user != request.user:  # type: ignore
+            return HttpResponseForbidden('You do not have the permission to edit this receipt')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('receipt_detail', args=[self.get_object().pk])
