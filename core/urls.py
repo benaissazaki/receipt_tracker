f3ee92c602a84a2ddbbb623c801954de8d487ab1
Zak Ben <zakben45@gmail.com>
2023-12-11 07:34:41 +0100

    Create receipt_list view

---
+++ core/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, reverse_lazy
+from django.contrib.auth.decorators import login_required
+
+from core.views import ReceiptListView
+
+urlpatterns = [
+    path('receipt', login_required(ReceiptListView.as_view(), login_url=reverse_lazy('login')), name='receipt_list')
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic.list import ListView
 
-# Create your views here.
+from .models import Receipt
+
+class ReceiptListView(ListView):
+    model = Receipt
+    paginate_by = 10
+    context_object_name = 'receipts'
+
+    def get_queryset(self) -> QuerySet[Receipt]:
+        return Receipt.objects.filter(user=self.request.user).order_by('date_of_purchase')
